3a1bd1564a74e0e11a80ce35b076e8dd738fd0c3
Kayinda Immaculé <immakaymw@gmail.com>
2024-02-09 02:59:49 +0100

    removing some useless coments

--- src/script.js
+++ src/script.js
@@ -1,6 +1,4 @@
 function toogleMenu(){
     const navList = document.querySelector(".nav__list");
-    console.log(navList.style)
     navList.classList.toggle("active")
-    // navList.style.display = (getComputedStyle(navList).display === "none" || navList.style.display === '') ? 'flex' : 'none';
 }
